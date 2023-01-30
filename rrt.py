f73f208e1f84c8c7d5a8717422e5379f158a7267
Jackson Bledsoe <jacksonbledsoe@gmail.com>
2023-01-30 15:55:50 +1000

    adding progress from vid 2

--- rrt.py
+++ rrt.py
@@ -11,21 +11,23 @@ def main():
     pygame.init()
     graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
     map=RRTMap(start,goal,dimensions,obsdim,obsnum)
-    # graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
 
     obstacles=graph.make_obs()
 
-    # map.draw_map(obstacles)
-
-    print("here")
     map.draw_map(obstacles)
 
     while(True):
         x,y = graph.sample_environment()
         n=graph.number_of_nodes()
         graph.add_node(n,x,y)
+        graph.add_edge(n-1,n)
+        x1,y1=graph.x[n],graph.y[n]
+        x2,y2=graph.x[n-1],graph.y[n-1]
         if(graph.is_free()):
             pygame.draw.circle(map.map, map.red, (graph.x[n], graph.y[n]), map.node_rad, map.node_thickness)
+            if not graph.cross_obstacle(x1,x2,y1,y2):
+                pygame.draw.line(map.map, map.blue,(x1,y1),(x2,y2),map.edge_thickness)
+
         pygame.display.update()
 
 
