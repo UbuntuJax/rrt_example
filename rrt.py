d073feb775c5607a3c0ceea2be58039b0ffe8ff1
Jackson Bledsoe <jacksonbledsoe@gmail.com>
2023-02-06 09:53:24 +1000

    updating rrt.py to vid4

--- rrt.py
+++ rrt.py
@@ -2,31 +2,44 @@ import pygame
 from rrtbasepy import RRTGraph, RRTMap
 
 def main():
+    iteration=0
     dimensions = (600,1000)
     start = (50,50)
-    goal = (510, 510)
+    goal = (800, 510)
     obsdim=30
     obsnum=50
 
     pygame.init()
     graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
     map=RRTMap(start,goal,dimensions,obsdim,obsnum)
-    # graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
 
     obstacles=graph.make_obs()
-
-    # map.draw_map(obstacles)
-
-    print("here")
     map.draw_map(obstacles)
 
-    while(True):
-        x,y = graph.sample_environment()
-        n=graph.number_of_nodes()
-        graph.add_node(n,x,y)
-        if(graph.is_free()):
-            pygame.draw.circle(map.map, map.red, (graph.x[n], graph.y[n]), map.node_rad, map.node_thickness)
-        pygame.display.update()
+
+    while (not graph.path_to_goal()):
+        if iteration % 5 ==0:
+            print("bias")
+            x,y,parent=graph.bias(goal)
+            pygame.draw.circle(map.map, map.grey,(x[-1],y[-1]),map.node_rad+2,0)
+            pygame.draw.line(map.map,map.blue,(x[-1],y[-1]),(x[parent[-1]],y[parent[-1]]),\
+                map.edge_thickness)
+
+        else:
+            print("expand")
+            x,y,parent=graph.expand()
+            pygame.draw.circle(map.map, map.grey, (x[-1], y[-1]), map.node_rad+2,0)
+            pygame.draw.line(map.map,map.blue,(x[-1],y[-1]),(x[parent[-1]],y[parent[-1]]),\
+                map.edge_thickness)
+
+        if iteration%5==0:
+            pygame.display.update()
+        iteration+=1
+    
+    map.draw_path(graph.get_path_coords())
+    pygame.display.update()
+    pygame.event.clear()
+    pygame.event.wait(0)
 
 
 
